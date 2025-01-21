800176a9af40180849fd52c910c4cf7474efc906
00elisangela00 <elisangelacristina@rocketmail.com>
2025-01-21 16:35:31 +0000

    Em andamento

--- SuperTrunfo2.c
+++ SuperTrunfo2.c
@@ -30,38 +30,10 @@ int main() {
 
 
     
-}
 
 
 
-int main (){
-    float temperatura, umidade;
-    unsigned int estoque, estoqueMinimo = 1000;
 
-    printf("Entre com a temperatura: \n");
-    scanf("%f", &temperatura);
-    printf("Entre com a umidade: \n");
-    scanf("%f", &umidade);
-    printf("Entre com o estoque: \n");
-    scanf("%u", &estoque);
-
-    if (temperatura > 30){
-        printf ("Temperatura está alta\n");
-    } else {
-        printf("Temperatura está dentro dos parâmetros\n");
-    }
-
-    if (umidade > 50){
-        printf ("Úmidade elevada\n");
-    } else {
-        printf("Úmidade está dentro dos parâmetros\n");
-    }
-
-    if (estoque < estoqueMinimo){
-        printf ("Estoque abaixo do minímo!\n");
-    } else {
-        printf("Estoque normal!\n");
-    }
 
 
 
