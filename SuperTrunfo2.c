39d477aa9148994761bcb4202e87b32332c5c4a8
00elisangela00 <elisangelacristina@rocketmail.com>
2025-01-21 21:45:14 +0000

    Em andamento 2

--- SuperTrunfo2.c
+++ SuperTrunfo2.c
@@ -10,13 +10,13 @@ int main() {
     float pib1;
     int pontos_turisticos1;
     
-    char cidade2 [30];
-    float populacao2;
-    float area2;
-    float pib2;
-    int pontos_turisticos2;
+    char Cidade2 [30];
+    float Populacao2;
+    float Area2;
+    float Pib2;
+    int Pontos_turisticos2;
  
-    printf("Escolha uma Cidade:\n");
+    printf("Escolha uma Cidade, esta será a Cidade 1:\n");
     scanf("%s",cidade1);
     printf("Qual a População?\n");
     scanf("%f", &populacao1);
@@ -24,7 +24,7 @@ int main() {
     scanf("%f", &area1);
     printf("Valor do PIB?\n");
     scanf("%f", &pib1);
-      printf("Quantidade de Pontos Turisticos?\n");
+    printf("Quantidade de Pontos Turisticos?\n");
     scanf("%d", &pontos_turisticos1);
 
     printf("Esta é sua carta:\n");
@@ -33,44 +33,51 @@ int main() {
     printf("Área: %f\n", area1);
     printf("PIB: %f\n", pib1);
     printf("Pontos Turisticos: %d\n", pontos_turisticos1);
-    
-    printf("Agora escolha outra cidade!\n");
-    scanf("%s", cidade2);
+
+    printf("Agora escolha outra cidade, esta será a Cidade 2:\n");
+    scanf("%s",Cidade2);
     printf("Qual a População?\n");
-    scanf("%f", &populacao2);
+    scanf("%f", &Populacao2);
     printf("Qual a Área?\n");
-    scanf("%f", &area2);
+    scanf("%f", &Area2);
     printf("Valor do PIB?\n");
-    scanf("%f", &pib2);
+    scanf("%f", &Pib2);
       printf("Quantidade de Pontos Turisticos?\n");
-    scanf("%d", &pontos_turisticos2);
+    scanf("%d", &Pontos_turisticos2);
 
     printf("Esta é sua carta:\n");
-    printf("Cidade: %s \n", cidade2);
-    printf("População: %f\n", populacao2);
-    printf("Área: %f\n", area2);
-    printf("PIB: %f\n", pib2);
-    printf("Pontos Turisticos: %d\n", pontos_turisticos2);
-}
+    printf("Cidade: %s \n", Cidade2);
+    printf("População: %f\n", Populacao2);
+    printf("Área: %f\n", Area2);
+    printf("PIB: %f\n", Pib2);
+    printf("Pontos Turisticos: %d\n", Pontos_turisticos2);
+
 
     printf("Hora de comparar as cartas\n");
 
-    if (populacao1 >= populacao2){
-        printf ("A cidade1 tem uma população maior que a cidade2\n");
+    
+    if (populacao1 > Populacao2){
+        printf("A Cidade 1 tem a maior população.\n");
     } else {
-        printf("A cidade2 tem uma população maior que a cidade1\n");
+        printf("A Cidade 2 tem a maior população.\n");
     }
 
-    if (area1 >= area2){
-        printf ("A cidade1 tem uma área maior que a cidade2\n");
+    if (area1 > Area2){
+        printf ("A Cidade 1 tem a maior área.\n");
     } else {
-        printf("A cidade2 tem uma área maior que a cidade1\n");
+        printf("A Cidade 2 tem a maior área.\n");
     }
 
-    if (pib1 >= pib2){
-        printf ("O PIB da cidade1 é maior que o da cidade2\n");
+    if (pib1 > Pib2){
+        printf ("O PIB da cidade1 é maior que o da Cidade2\n");
     } else {
-        printf("O PIB da cidade2 é maior que o da cidade1\n");
+        printf("O PIB da Cidade2 é maior que o da cidade1\n");
+    }
+
+    if (pontos_turisticos1 > Pontos_turisticos2){
+        printf("A Cidade 1 tem mais Pontos Turísticos.\n");
+    } else {
+        pprintf("A Cidade 2 tem mais Pontos Turísticos.\n");
     }
     
 
