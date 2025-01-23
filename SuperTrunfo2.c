24132f5e42d516d5b1975830b55bf9c628bff45b
00elisangela00 <elisangelacristina@rocketmail.com>
2025-01-23 19:10:44 +0000

    Finalizado

--- SuperTrunfo2.c
+++ SuperTrunfo2.c
@@ -16,10 +16,8 @@ int main() {
     float Pib2;
     int Pontos_turisticos2;
  
-    char cidadeVencedora = populacao1+area1+pib1+pontos_turisticos1 > Populacao2+Area2+Pib2+Pontos_turisticos2;
-
     printf("Escolha uma Cidade, esta será a Cidade 1:\n");
-    scanf("%s",cidade1);
+    scanf("%s",cidade1);//Não consigo responder com nomes composto no scanf
     printf("Qual a População?\n");
     scanf("%f", &populacao1);
     printf("Qual a Área?\n");
@@ -29,6 +27,7 @@ int main() {
     printf("Quantidade de Pontos Turisticos?\n");
     scanf("%d", &pontos_turisticos1);
 
+    printf("\n"); //Utilizei mais espaço para facilitar a visualização
     printf("Esta é sua carta:\n");
     printf("Cidade: %s \n", cidade1);
     printf("População: %f\n", populacao1);
@@ -36,6 +35,7 @@ int main() {
     printf("PIB: %f\n", pib1);
     printf("Pontos Turisticos: %d\n", pontos_turisticos1);
 
+    printf("\n");
     printf("Agora escolha outra cidade, esta será a Cidade 2:\n");
     scanf("%s",Cidade2);
     printf("Qual a População?\n");
@@ -44,9 +44,10 @@ int main() {
     scanf("%f", &Area2);
     printf("Valor do PIB?\n");
     scanf("%f", &Pib2);
-      printf("Quantidade de Pontos Turisticos?\n");
+    printf("Quantidade de Pontos Turisticos?\n");
     scanf("%d", &Pontos_turisticos2);
 
+    printf("\n");
     printf("Esta é sua carta:\n");
     printf("Cidade: %s \n", Cidade2);
     printf("População: %f\n", Populacao2);
@@ -54,42 +55,38 @@ int main() {
     printf("PIB: %f\n", Pib2);
     printf("Pontos Turisticos: %d\n", Pontos_turisticos2);
 
-
+    printf("\n");
     printf("Hora de comparar as cartas\n");
-
-    
     if (populacao1 > Populacao2){
-        printf("A Cidade 1 tem a maior população.\n");
+        printf("A Cidade 1 tem a maior População.\n");
     } else {
-        printf("A Cidade 2 tem a maior população.\n");
+        printf("A Cidade 2 tem a maior População.\n");
     }
-
     if (area1 > Area2){
-        printf ("A Cidade 1 tem a maior área.\n");
+        printf ("A Cidade 1 tem a maior Área.\n");
     } else {
-        printf("A Cidade 2 tem a maior área.\n");
+        printf("A Cidade 2 tem a maior Área.\n");
     }
-
     if (pib1 > Pib2){
         printf("A Cidade 1 tem o maior PIB.\n");
     } else {
         printf("A Cidade 2 tem o maior PIB.\n");
     }
-
     if (pontos_turisticos1 > Pontos_turisticos2){
         printf("A Cidade 1 tem mais Pontos Turísticos.\n");
     } else {
         printf("A Cidade 2 tem mais Pontos Turísticos.\n");
     }
+
+    float cidadeVencedora = populacao1 + area1 + pib1 + pontos_turisticos1; //Seria certo colocar o Array aqui embaixo mesmo? Pois quando ele estava em cima não rodava o resultado direito.
+    float cidadeVencedora2 = Populacao2 + Area2 + Pib2 + Pontos_turisticos2;
     
-printf("A cidade vencedora é: %s\n", cidadeVencedora)
-    if (cidade1 > Cidade2){
-        printf("A vencedora é a Cidade 1!\n");
+    printf("\n");
+    if (cidadeVencedora > cidadeVencedora2){
+        printf("A vencedora é a Cidade 1: %s", cidade1);
     } else {
-        printf("A vencedora é a Cidade 2!\n");
+        printf("A vencedora é a Cidade 2: %s", Cidade2);
     }
-
-
 
 
 return 0;
